5740ec183a41637208e0b93bee262bcd4e74eabd
uvorbs <yuval@orbs.com>
2022-08-04 15:06:04 +0300

    nginx conf

--- install.sh
+++ install.sh
@@ -12,6 +12,9 @@ echo "== ORBS V3 Node Setup - INSTALL DOCKER"
 echo "======================================\n\n"
 wget -O - https://gist.githubusercontent.com/fredhsu/f3d927d765727181767b3b13a3a23704/raw/3c2c55f185e23268f7fce399539cb6f1f3c45146/ubuntudocker.sh | bash
 
+# init swarm mode
+docker swarm init
+
 echo "============================================="
 echo "== ORBS V3 Node Setup - INSTALL NODE EXPORTER"
 echo "=============================================\n\n"
