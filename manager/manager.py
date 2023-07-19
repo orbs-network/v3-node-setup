f2dc96eee14df591ae22aeb294fbf0ef472adf71
Luke Rogerson <luke@orbs.com>
2023-07-19 15:08:00 +0100

    Update manager run_command func to return real time output of command

--- manager/manager.py
+++ manager/manager.py
@@ -1,6 +1,7 @@
-import os
-import subprocess
 import json
+import select
+import subprocess
+import sys
 from datetime import datetime
 from typing import Optional, Tuple
 
@@ -8,25 +9,43 @@ errors_file = '/opt/orbs/errors.txt'
 node_version = '/opt/orbs/node-version.json'
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-def run_command(command: str) -> Tuple[Optional[str], Optional[str]]:
-    """Runs a shell command and returns the output and error message (if any)
+def run_command(command: str) -> Optional[str]:
+    """Runs a shell command and returns the error message (if any).
 
     Args:
-        command: The command to run (eg. `docker-compose up -d`)
+        command: The command to run (eg. `docker-compose up -d`).
 
     Returns:
-        A tuple containing the output and error message (if any)
+        The error message (if any).
     """
-    result = subprocess.run(command, shell=True, capture_output=True)
-    
-    if result.returncode != 0:
-        error_message = result.stderr.decode("utf-8")
-        with open(errors_file, "a") as f:
-            f.write(f"{timestamp} - {error_message.strip()}\n")
-        return None, error_message
-    
-    output = result.stdout.decode("utf-8")
-    return output, None
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+    while True:
+        # Use select for non-blocking I/O on both stdout and stderr
+        ready_to_read, _, _ = select.select([process.stdout, process.stderr], [], [])
+        for output in ready_to_read:
+            line = output.readline().strip()
+
+            # Print output in real-time
+            if line:
+                print(line)
+
+        # Check for termination
+        if process.poll() is not None:
+            # Process has finished, read rest of the output 
+            for output in [process.stdout, process.stderr]:
+                for line in output.readlines():
+                    line = line.strip()
+                    if line:
+                        print(line)
+
+            if process.returncode != 0:
+                with open(errors_file, "a") as f:
+                    error_message = f"Command '{command}' returned non-zero exit status {process.returncode}"
+                    f.write(f"{timestamp} - {error_message}\n")
+                return error_message
+            
+            return None
         
 
 # TODO - add back when we split into seperate repos
@@ -48,7 +67,7 @@ data["lastUpdated"] = timestamp
 if latest_tag and latest_tag != data["currentVersion"]:
     # checkout_command = f"git checkout {latest_tag}"
     # run_command(checkout_command)  # checkout the latest tag
-    _, error = run_command("docker-compose -f $HOME/deployment/docker-compose.yml up -d")
+    error = run_command("docker-compose -f $HOME/deployment/docker-compose.yml up -d")
     if error:
         print("Error running docker-compose")
     
