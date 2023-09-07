535e8fffc6bf4b3e0c18e0b7722542a51fc06650
uvorbs <yuval@orbs.com>
2023-09-07 13:48:37 +0300

    install manager cron for current user

--- setup/scripts/setup-manager.sh
+++ setup/scripts/setup-manager.sh
@@ -13,7 +13,7 @@ echo "------------------------------------"
 echo -e "${BLUE}Adding scheduled manager run...${NC}"
 
 chmod +x $HOME/manager/manager.py
-sudo crontab $HOME/setup/deployment-poll.cron
+sudo crontab $HOME/setup/deployment-poll.cron -u $username
 sudo service cron restart
 
 echo -e "${GREEN}Manager schedule set!${NC}"
