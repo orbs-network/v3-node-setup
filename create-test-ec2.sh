664c10ad8cba66ec644bfa4e7642a729b248fa5b
Luke Rogerson <contact@lukerogerson.me>
2023-09-14 13:23:31 +0100

    Fix Ubuntu apt-get error (#31)
    Fix apt-get error
    Cleanup and misc improvements

--- create-test-ec2.sh
+++ create-test-ec2.sh
@@ -2,11 +2,10 @@
 
 # --- Quick script to create an EC2 instance with the correct specs and security group ---
 
-# Change this to your desired instance name
-instanceName=
+read -p "Please enter your desired instance name: " instanceName
 
 if [ -z "${instanceName}" ]; then
-    echo "ERROR: Must set instanceName"
+    echo "ERROR: instanceName cannot be empty"
     exit 1
 fi
 
@@ -19,3 +18,16 @@ securityGroupId="sg-0445211f967de7058"
 # aws ec2 authorize-security-group-ingress --group-id ${securityGroupId} --protocol tcp --port 9100 --cidr 0.0.0.0/0
 
 aws ec2 run-instances --image-id ami-053b0d53c279acc90 --count 1 --instance-type t2.xlarge --key-name v3-test --block-device-mappings '[{"DeviceName":"/dev/sda1", "Ebs":{"VolumeSize":30, "DeleteOnTermination":true, "VolumeType":"gp2", "SnapshotId":"snap-0d3283808e9f92122"}}]' --network-interfaces '[{"DeviceIndex":0, "AssociatePublicIpAddress":true, "Groups":["'${securityGroupId}'"]}]' --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="'${instanceName}'"}]'
+
+
+sleep 5
+
+instancePublicIp=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${instanceName}" --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+
+if [ -z "$instancePublicIp" ]; then
+    echo "Connect to your instance ${instanceName} from the AWS console"
+else
+    sshConnectionString="ssh -i \"v3-test.pem\" ubuntu@${instancePublicIp}"
+    echo "Use the following command to connect to your instance:"
+    echo -e "\033[1m$sshConnectionString\033[0m"
+fi
