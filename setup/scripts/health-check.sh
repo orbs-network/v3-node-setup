a7cf4f3e24894bb72b98c4700db80c4f8dd54743
idanatar <idana@orbs.com>
2023-09-19 14:47:50 +0300

    fixed health-check.sh

--- setup/scripts/health-check.sh
+++ setup/scripts/health-check.sh
@@ -1,45 +1,34 @@
 #!/bin/bash
 
-COMPOSE_FILE="$HOME/deployment/docker-compose.yml"
+check_services() {
+  compose_file="$HOME/deployment/docker-compose.yml"
+  # Get the number of services defined in docker-compose file
+  num_services=$(docker-compose -f $compose_file config --services | wc -l)
 
-echo -e "${BLUE}Performing a health check...${NC}\n"
+  for i in {1..5}
+  do
+    # Get the number of services that are up
+    num_up=$(docker-compose -f $compose_file ps | grep "Up" | wc -l)
+    if [ $num_up -eq $num_services ]; then
+      echo "All services are up and running."
+      return 0
+    else
+      echo "Waiting for services to start..."
+      sleep 5
+    fi
+  done
+  return 1
+}
 
-# Get the number of services defined in docker-compose file
-num_services=$(docker-compose -f $COMPOSE_FILE config --services | wc -l)
-# Wait for services to be up
-while true
-do
-  # Get the number of services that are up
-  num_up=$(docker-compose -f $COMPOSE_FILE ps | grep "Up" | wc -l)
-
-  if [ $num_up -eq $num_services ]; then
-    echo -e "${GREEN}Installation complete! 🚀🚀🚀${NC}"
-    break
+if check_services; then
+  mgmt_svs_status_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/service/ethereum-reader/status)
+  if [ $mgmt_svs_status_code -eq 200 ]; then
+      echo -e "${GREEN}Installation complete! 🚀🚀🚀${NC}"
+      echo "------------------------------------"
+      echo -e "\n👉👉👉 ${YELLOW}Please register your Guardian using the following website: https://guardians.orbs.network?name=$name&website=$website&ip=$myip&node_address=$public_add ${NC} 👈👈👈\n" # TODO: only show once - during first installation
   else
-    echo "Waiting for services to start..."
-    podman ps -a
-    echo "------------------------------------"
-    podman logs logger
-    podman inspect logger
-    echo "------------------------------------"
-    podman logs nginx
-    podman inspect nginx
-    echo "------------------------------------"
-    podman logs ethereum-writer
-    podman inspect ethereum-writer
-    sleep 5
+      echo -e "${RED}Installation incomplete!${NC}"
   fi
-done
-echo "------------------------------------"
-echo -e "\n👉👉👉 ${YELLOW}Please register your Guardian using the following website: https://guardians.orbs.network?name=$name&website=$website&ip=$myip&node_address=$public_add ${NC} 👈👈👈\n" # TODO: only show once - during first installation
-
-podman ps -a
-
-#mgmt_svs_status_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/service/ethereum-reader/status)
-#if [ $mgmt_svs_status_code -eq 200 ]; then
-#    echo -e "${GREEN}Installation complete! 🚀🚀🚀${NC}"
-#    echo "------------------------------------"
-#    echo -e "\n👉👉👉 ${YELLOW}Please register your Guardian using the following website: https://guardians.orbs.network?name=$name&website=$website&ip=$myip&node_address=$public_add ${NC} 👈👈👈\n" # TODO: only show once - during first installation
-#else
-#    echo -e "${RED}Installation incomplete!${NC}"
-#fi
+else
+  echo -e "${RED}Installation incomplete!${NC}"
+fi
