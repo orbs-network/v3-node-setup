61b2ce00952c9b8be492944e45cc46564d1d2aa4
Jordan Sheinfeld <jordan.sheinfeld@gmail.com>
2024-11-20 17:18:01 +0200

    Use orbs_root

--- setup/install.sh
+++ setup/install.sh
@@ -3,7 +3,7 @@
 
 export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-source $SCRIPT_DIR/base.sh
+source $SCRIPT_DIR/scripts/base.sh
 
 echo -e "${BLUE}
       ██████╗ ██████╗ ██████╗ ███████╗
