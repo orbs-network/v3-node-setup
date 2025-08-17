b8bc671daa3e4fdaee16ea7a5ff718d7dd0c6008
uvorbs <yuval@orbs.com>
2025-08-17 11:22:43 +0300

    nicer registration output

--- setup/scripts/health-check.sh
+++ setup/scripts/health-check.sh
@@ -25,9 +25,20 @@ if check_services; then
   if [ $mgmt_svs_status_code -eq 200 ]; then
       echo -e "${GREEN}Installation complete! 🚀🚀🚀${NC}"
       echo "------------------------------------"
-      echo -e "\n👉👉👉 ${YELLOW}Please register your Guardian using the following website: https://guardians.orbs.network?name=$name&website=$website&ip=$myip&node_address=$public_add ${NC} 👈👈👈\n" # TODO: only show once - during first installation
+      echo -e "👉👉👉 ${YELLOW}Please register your Guardian using:"
+      echo -e "website: https://guardians.orbs.network?name=$name&website=$website&ip=$myip&node_address=$public_add=${NC}" 
+      echo -e "👈👈👈"
+      echo -e "name: $name"
+      echo -e "node_address: $public_add"
+      echo -e "website: $website"
+      echo -e "ip: $myip"                
+      echo -e "------------------------------------"      
   else
       echo -e "${RED}Installation incomplete!${NC}"
+      #
+      echo -e "Please check the logs for more information."
+      # todo: add logs path
+      echo -e "------------------------------------"
   fi
 else
   echo -e "${RED}Installation incomplete!${NC}"
